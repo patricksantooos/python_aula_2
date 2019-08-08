f74de4a0f3774972b6f216e40089037ebb4cc51d
patricksantooos <trickpa2506@gmail.com>
2019-08-08 20:30:56 -0300

    first commit

---
+++ Ex01.py
@@ -0,0 +1,29 @@
+#idade = input("Digite a idade: ")
+
+class Ex01():
+
+  def testarIdade(self):
+    idade = input("Digite a idade: ")
+    if int(idade) > 18:
+      print("Maior de 18 anos")
+    elif int(idade) < 18:
+      print("Menor de 18")
+    else:
+      print("Igual a 18")
+
+  testarIdade()
+
+def calculaMedia():
+  lista = []
+  for i in range(5):
+    aluno = input("Digite o nome do aluno: ")
+    for i  in range(3):
+      nota = float(input("Digite  nota {}: ".format(i+1)))
+      lista.append(nota)
+
+    media = sum(lista)/3
+
+    if float(media) >= 7:
+      print("{} está Aprovado!".format(aluno))
+    else:
+      print("{} está Reprovado!".format(aluno))
